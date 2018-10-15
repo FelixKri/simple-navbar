5375c356d18df0471b9466fb640223d6f07d25f2
Felix Kristandl <felix.kristandl@outlook.com>
2018-10-15 21:58:13 +0200

    added some animations for the hamburger menu

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 function expandNavbar(){
     var list = document.getElementById("nav-list");
+
+    var p1 = document.getElementById("p1");
+    var p2 = document.getElementById("p2");
+    var p3 = document.getElementById("p3");
+
     if(list.style.display==="block"){
         list.style.display = "none";
+        p2.style.opacity = "1";
+
+        p1.style.transform = "rotate(0deg)";
+        p3.style.transform = "rotate(0deg)";
     }else{
-        console.log("display = none");
+        p2.style.opacity="0";
         list.style.display = "block";
+
+        p1.style.transform = "rotate(45deg)";
+        p1.style.transformOrigin = "top left";
+
+        p3.style.transform = "rotate(-45deg)";
+        p3.style.transformOrigin = "bottom left";
+
     }
 }
